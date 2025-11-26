9b29438490f9f32f1645df90a688379700097a5e
Alexandre Wetzel <alexwtz@gmail.com>
2025-11-26 21:52:11 +0100

    Ajout déplacement mail en erreur dans un dossier erreur

--- sync_elm.py
+++ sync_elm.py
@@ -7,6 +7,8 @@ import os
 from email import policy
 from email.parser import BytesParser
 from email.header import decode_header, make_header
+import shutil
+
 load_dotenv()
 
 IMAP_SERVER = "127.0.0.1"  # pour Proton Mail Bridge : 127.0.0.1
@@ -15,6 +17,7 @@ USERNAME = os.getenv("USR")
 PASSWORD = os.getenv("PWD")
 EML_FOLDER = os.getenv("EML_FOLDER")
 EML_ARCHIVE = os.getenv("EML_ARCHIVE")
+EML_ERROR = os.getenv("EML_ERROR")
 
 #MAILBOX = '"All Mail"'          # boîte à nettoyer
 MAILBOX="INBOX"
@@ -68,6 +71,7 @@ def get_existing_message_ids(mail):
     uids = data[0].split()
     message_ids = set()
 
+    '''
     for uid in uids:
         result, msg_data = mail.fetch(uid, "(RFC822)")
         if result != "OK":
@@ -79,14 +83,49 @@ def get_existing_message_ids(mail):
         if msgid:
             message_ids.add(msgid.strip())
 
+    
+    '''
+    # batch size: fetch 500 at a time (tune as you like)
+    batch_size = 2500
+    batch_id = 1
+    for i in range(0, len(uids), batch_size):
+        print(f"Fetching batch {batch_id}/{int(round(len(uids)/batch_size,0))}")
+        batch_id = batch_id + 1
+        batch = b",".join(uids[i:i + batch_size])
+
+        # fetch only the header, not whole mail
+        result, msg_data = mail.fetch(
+            batch,
+            "(BODY.PEEK[HEADER.FIELDS (MESSAGE-ID)])"
+        )
+
+        if result != "OK":
+            continue
+
+        # parse each header block
+        for part in msg_data:
+            if isinstance(part, tuple):
+                header = part[1]
+                try:
+                    msg = email.message_from_bytes(header)
+                    msgid = msg.get("Message-ID")
+                    if msgid:
+                        message_ids.add(msgid.strip())
+                except Exception as e:
+                    print(e)
+
     print(f"[+] Existing messages: {len(message_ids)}")
     return message_ids
 
 def import_eml_if_new(mail, existing_ids,restart=-1):
     cnt_mail = 0
     nb_file = len(os.listdir(EML_FOLDER))
-    for filename in os.listdir(EML_FOLDER):
+    print(f"{nb_file} mails found")
+    list = os.listdir(EML_FOLDER)
+    #list.reverse()
+    for filename in list:
         try:
+            error = False
             cnt_mail = cnt_mail + 1
             if cnt_mail % 1000 == 0:
                 print(f"Already checked {cnt_mail}/{nb_file} e-mails")
@@ -117,11 +156,7 @@ def import_eml_if_new(mail, existing_ids,restart=-1):
                 #    print(f"[=] SKIP (already exists): {filename}")
                 #except:
                 #    print(f"[=] SKIP (already exists): error on filename code")
-                import shutil
-
-
                 dst = os.path.join(EML_ARCHIVE, filename)
-
                 shutil.move(filepath, dst)
                 continue
 
@@ -131,8 +166,10 @@ def import_eml_if_new(mail, existing_ids,restart=-1):
             #    print(f"[+] Importing: error on filename code")
 
             # Upload (APPEND) to Proton Mail
-
-            raw = fix_eml_headers(raw)
+            try:
+                raw = fix_eml_headers(raw)
+            except:
+                print("error fixing header")
             result = mail.append(MAILBOX, r"(\Seen)", None, raw)
             if result[0] == "OK":
                 try:
@@ -141,12 +178,21 @@ def import_eml_if_new(mail, existing_ids,restart=-1):
                     print(f"[✓] Imported: error on filename code")
                 existing_ids.add(msgid)
             else:
+                error=True
                 try:
                     print(f"[!] FAILED to import: {filename} — {result}")
                 except:
                     print(f"[!] FAILED to import: error on filename code")
+            if error:
+                dst = os.path.join(EML_ERROR, filename)
+                shutil.move(filepath, dst)
+                print(f"file move to {dst}")
+
         except Exception as e:
             print(f"[!] Failed to import: {e}")
+            dst = os.path.join(EML_ERROR, filename)
+            shutil.move(filepath, dst)
+            print(f"file move to {dst}")
             mail = imaplib.IMAP4(IMAP_SERVER, IMAP_PORT)
             mail.login(USERNAME, PASSWORD)
 
@@ -156,7 +202,9 @@ def main():
     mail = imaplib.IMAP4(IMAP_SERVER, IMAP_PORT)
     mail.login(USERNAME, PASSWORD)
 
-    if os.path.exists("existing_ids.json"):
+    relaod = True
+
+    if not relaod and os.path.exists("existing_ids.json"):
         existing_ids = set(json.load(open("existing_ids.json")))
     else:
         existing_ids = get_existing_message_ids(mail)
